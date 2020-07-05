cd972b1cdde5a68024f2babe1aaa918a4b24b733
guptamo <mo@mohitgupta.ca>
2020-07-05 00:16:57 -0400

    add contest model

--- potw/contests/models.py
+++ potw/contests/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Contest(models.Model):
+    theme = models.CharField(max_length=280)
+    pick_deadline = models.DateTimeField()
+    vote_deadline = models.DateTimeField()
+    admin_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    spotify_playlist = models.SlugField()
+    youtube_playlist = models.SlugField()
+
+    def get_status(self):
+        if datetime.utcnow() < pick_deadline:
+            return 'picking'
+        elif pick_deadline < datetime.utcnow() < vote_deadline:
+            return 'voting'
+        else:
+            return 'closed'
+
+    def __str__(self):
+        return f'Contest {self.id}: {self.theme}'
+
