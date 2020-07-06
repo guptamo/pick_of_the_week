42fbbef8fcd42f029eb8b28a6da0efc33c59ea1a
guptamo <mo@mohitgupta.ca>
2020-07-05 23:14:57 -0400

    add Pick model

--- potw/contests/models.py
+++ potw/contests/models.py
@@ -21,3 +21,13 @@ class Contest(models.Model):
     def __str__(self):
         return f'Contest {self.id}: {self.theme}'
 
+
+class Pick(models.Model):
+    picked_by = models.CharField(max_length=280)
+    artist = models.CharField(max_length=280)
+    song = models.CharField(max_length=280)
+    spotify_track = models.URLField(null=True)
+    youtube_video = models.URLField(null=True)
+
+    def __str__(self):
+        return f'{self.song} picked by {self.picked_by}'
