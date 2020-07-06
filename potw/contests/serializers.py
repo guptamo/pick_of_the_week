2af3a4d37202f88425d8dfeb96d613b9d7739af8
guptamo <mo@mohitgupta.ca>
2020-07-05 23:27:08 -0400

    add Pick serializer

--- potw/contests/serializers.py
+++ potw/contests/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Contest
+from .models import Contest, Pick
 
 class ContestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contest
+        fields = '__all__'
+
+class PickSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pick
         fields = '__all__'
