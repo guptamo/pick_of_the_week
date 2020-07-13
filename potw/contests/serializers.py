960122d9755ca458f907a4a81174cc7ec864fb52
guptamo <mo@mohitgupta.ca>
2020-07-12 23:18:24 -0400

    update contest serializer  - add fields for pick_set, and status

--- potw/contests/serializers.py
+++ potw/contests/serializers.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
 from .models import Contest, Pick
 
-class ContestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contest
-        fields = '__all__'
 
 class PickSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pick
         fields = '__all__'
+
+
+class ContestSerializer(serializers.ModelSerializer):
+    pick_set = PickSerializer(many=True, read_only=True)
+    status = serializers.CharField(source='get_status', read_only=True)
+
+    class Meta:
+        model = Contest
+        fields = ['theme',
+                   'pick_deadline',
+                   'vote_deadline',
+                   'admin_id',
+                   'spotify_playlist',
+                   'youtube_playlist',
+                   'pick_set',
+                   'status']
